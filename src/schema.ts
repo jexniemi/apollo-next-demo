360ee642f6d445ca87d5d28035ea81b00105ff4f
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2024-10-03 07:36:25 +0300

    remove author

--- src/schema.ts
+++ src/schema.ts
@@ -8,20 +8,15 @@ const typeDefs = `#graphql
   # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
-    author: string
+    author: String
   }
 
-"""   type Author {
-    name: String! # Can't return null
-    books: [Book]
-  }
- """
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book!]! # Outter "!"" = List can't be null, inner "!" = item can't be null
-    """ authors: [Author] """
   }
 `;
 
