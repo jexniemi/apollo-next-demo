068b6e49c4ff410fe76be4e7e6a5e0c7544fd91d
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2024-10-04 14:05:24 +0300

    edit front page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,21 +3,14 @@ import BookList from "./components/BookList";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 text-center">
+        <h1 className="text-3xl">Books</h1>
         <div>
           <BookList />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 mt-auto flex flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="http://localhost:4000/graphql"
